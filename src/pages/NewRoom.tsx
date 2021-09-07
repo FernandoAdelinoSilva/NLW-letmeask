f6fbaf8be89dc75a16eebec4c2b77a5487707ac5
fernandoadelinosilva <fernandoadelinodasilva@hotmail.com>
2021-09-07 16:43:03 -0300

    feat(Home): Enable Authentication

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,33 +1,42 @@
+import { Link } from 'react-router-dom'
+
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss'
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
-    return (
-        <div id="page-auth">
-					<aside>
-						<img src={ilustrationImg} alt="Ilustration QA"/>
-						<strong>Create Live Rooms for Q&amp;A</strong>
-						<p>Answer Real Time Questions</p>
-					</aside>
-					<main>
-						<div className="main-content">
-							<img src={logoImg} alt="Letmeask" />
-              <h2>Create a new Room</h2>
-							<div className="separator">Or choose a room</div>
-							<form >
-								<input 
-									type="text"
-									placeholder="room name"
-								/>
-								<Button type="submit">
-									Create Room
-								</Button>
-							</form>
-						</div>
-					</main>
-        </div>
-    )
+
+	const { user } = useAuth();
+	console.log(user?.name);
+
+	return (
+			<div id="page-auth">
+				<aside>
+					<img src={ilustrationImg} alt="Ilustration QA"/>
+					<strong>Create Live Rooms for Q&amp;A</strong>
+					<p>Answer Real Time Questions</p>
+				</aside>
+				<main>
+					<div className="main-content">
+						<img src={logoImg} alt="Letmeask" />
+						<h2>Create a new Room</h2>
+						<div className="separator">Or choose a room</div>
+						<form >
+							<input 
+								type="text"
+								placeholder="room name"
+							/>
+							<Button type="submit">
+								Create Room
+							</Button>
+						</form>
+						<p>
+							Want to Join in a existing room ? <Link to="/">click here</Link>
+						</p>
+					</div>
+				</main>
+			</div>
+	)
 }
